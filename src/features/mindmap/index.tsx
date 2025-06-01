93084bb1c6d818055fcbac08f662d5a7be25c853
yopi416 <yoppytennis416@gmail.com>
2025-06-01 23:30:23 +0900

    mindmap for dummynode #3

--- src/features/mindmap/index.tsx
+++ src/features/mindmap/index.tsx
@@ -1,10 +1,36 @@
-import { ReactFlow, Background, Controls } from '@xyflow/react'
+import { ReactFlow, Background, Controls, type NodeOrigin } from '@xyflow/react'
+
+import { useShallow } from 'zustand/shallow'
+import useMindMapStore, { type MindMapStore } from './store'
+
 import '@xyflow/react/dist/style.css'
 
-function Flow() {
+const selector = (state: MindMapStore) => ({
+  nodes: state.nodes,
+  edges: state.edges,
+  onNodesChange: state.onNodesChange,
+  onEdgesChange: state.onEdgesChange,
+})
+
+// this makes the node origin to be in the center of a node
+const nodeOrigin: NodeOrigin = [0.5, 0.5]
+
+function MindMap() {
+  const { nodes, edges, onNodesChange, onEdgesChange } = useMindMapStore(
+    useShallow(selector)
+  )
+
   return (
     <div style={{ height: '100%' }}>
-      <ReactFlow>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        nodeOrigin={nodeOrigin}
+        nodesDraggable={false}
+        fitView
+      >
         <Background />
         <Controls />
       </ReactFlow>
@@ -12,4 +38,4 @@ function Flow() {
   )
 }
 
-export default Flow
+export default MindMap
