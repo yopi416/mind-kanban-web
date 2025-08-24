a6915c41b10cbfbe1c6e576c081a04da30712a91
yopi416 <yoppytennis416@gmail.com>
2025-08-24 23:57:38 +0900

    redo&undo #43

--- src/features/mindmap/utils/projectUtils.ts
+++ src/features/mindmap/utils/projectUtils.ts
@@ -17,16 +17,50 @@ export function getCurrentPj(getStore: () => MindMapStore) {
 
 // currentPjの変更をProjects全体に反映
 export function applyPjChanges(
-  getStore: () => MindMapStore,
+  // getStore: () => MindMapStore,
   setStore: typeof useMindMapStore.setState,
-  updater: (pj: Project) => Project //pjを投げると、あるプロパティを更新したcurrentPjを返す関数
+  updater: (pj: Project) => Project, //pjを投げると、あるプロパティを更新したcurrentPjを返す関数
+  isAddedToStack: boolean = false // UndoStackに追加されるかのフラグ
 ) {
-  const store = getStore()
-  const currentPj = getCurrentPj(getStore)
-  setStore({
-    projects: {
-      ...store.projects,
-      [store.currentPjId]: updater({ ...currentPj, updatedAt: now() }), //storeのcurrentPj部分をupdaterで取得した変更反映後pjに更新
-    },
+  setStore((prevStore) => {
+    const { projects: currentPjs, currentPjId, history } = prevStore
+    const currentPj = currentPjs[currentPjId]
+
+    if (!currentPj) {
+      throw new Error(`Project not found: ${prevStore.currentPjId}`)
+    }
+
+    if (isAddedToStack) {
+      const { undoStack, redoStack } = history
+      undoStack.push({ nodes: currentPj.nodes, edges: currentPj.edges })
+      redoStack.clear()
+
+      // undoStack追加パターン
+      return {
+        projects: {
+          ...currentPjs,
+          [currentPjId]: updater({ ...currentPj, updatedAt: now() }), //storeのcurrentPj部分をupdaterで取得した変更反映後pjに更新
+        },
+        undoCount: undoStack.size,
+        redoCount: redoStack.size,
+      }
+    }
+
+    // undoスタック追加しないパターン
+    return {
+      projects: {
+        ...currentPjs,
+        [currentPjId]: updater({ ...currentPj, updatedAt: now() }), //storeのcurrentPj部分をupdaterで取得した変更反映後pjに更新
+      },
+    }
   })
+
+  // const store = getStore()
+  // const currentPj = getCurrentPj(getStore)
+  // setStore({
+  //   projects: {
+  //     ...store.projects,
+  //     [store.currentPjId]: updater({ ...currentPj, updatedAt: now() }), //storeのcurrentPj部分をupdaterで取得した変更反映後pjに更新
+  //   },
+  // })
 }
