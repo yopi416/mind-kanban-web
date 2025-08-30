315e0b97e4974d914626b370031153631cc2b00c
yopi416 <yoppytennis416@gmail.com>
2025-08-30 23:53:23 +0900

    pj毎のhistory対応 #43

--- src/features/mindmap/utils/historyUtils.ts
+++ src/features/mindmap/utils/historyUtils.ts
@@ -1,4 +1,4 @@
-import type { Stack, StackItem, NodeData, History, SetStore } from '@/types'
+import type { StackItem, NodeData, History, HistoryByPj } from '@/types'
 import type { Node, Edge } from '@xyflow/react'
 
 //  Undo/Redo用 Utils
@@ -59,24 +59,77 @@ export const cloneSnapshot = (
 // }
 
 // 変更前状態をUndoStackに追加しつつ、変更後のカウンタを返す
-export function pushUndoItem(
-  history: History,
-  undoItem: StackItem
-): { undoCount: number; redoCount: number } {
-  const { undoStack, redoStack } = history
-  undoStack.push(undoItem)
-  redoStack.clear()
-  return { undoCount: undoStack.size, redoCount: redoStack.size }
+// export function pushUndoItem(
+//   history: History,
+//   undoItem: StackItem
+// ): { undoCount: number; redoCount: number } {
+//   const { undoStack, redoStack } = history
+//   undoStack.push(undoItem)
+//   redoStack.clear()
+//   return { undoCount: undoStack.size, redoCount: redoStack.size }
+// }
+
+// pushの際に、上限からあふれた場合はslideする
+export function pushToStack(
+  stack: StackItem[],
+  item: StackItem,
+  maxSize: number
+): StackItem[] {
+  const newStack = [...stack, item].slice(-maxSize)
+  return newStack
 }
 
-// undo/redo Stackのリセット
-export function clearHistory(history: History, setStore: SetStore) {
-  const { undoStack, redoStack } = history
-  undoStack.clear()
-  redoStack.clear()
+export function popFromStack(
+  stack: StackItem[]
+): [StackItem | undefined, StackItem[]] {
+  if (stack.length === 0) return [undefined, []]
 
-  setStore({
-    undoCount: undoStack.size,
-    redoCount: redoStack.size,
-  })
+  const lastIndex = stack.length - 1
+  const poppedItem = stack[lastIndex]
+  const restStack = stack.slice(0, -1)
+
+  return [poppedItem, restStack]
+}
+
+// pushに加えて、redoStackのclearも実施
+export function pushUndoItem(
+  history: History,
+  undoItem: StackItem,
+  maxSize: number
+): History {
+  const { undoStack } = history
+  const newUndoStack = pushToStack(undoStack, undoItem, maxSize)
+  return { undoStack: newUndoStack, redoStack: [] }
+}
+
+// 初ロード時 or PJ変更時に使用
+// historyByProjectの初期値は{}なので、[currentPjId]がundefined
+// その際にcurrentPjIdをキーとする、空Historyを作成する
+export function createEmptyHistory(): History {
+  return {
+    undoStack: [],
+    redoStack: [],
+  }
+}
+
+// currentPjに対応するhistoryを読み込む
+// その際に、存在しなければ空stackを作成
+export function getCurrentHistory(
+  historyByPj: HistoryByPj,
+  pjId: string
+): History {
+  const currentHistory = historyByPj[pjId] ?? createEmptyHistory()
+  return currentHistory
+}
+
+// currentPjに対応するhistoryを更新
+export function updateHistoryMap(
+  historyByPj: HistoryByPj,
+  currentPjId: string,
+  newHistory: History
+): HistoryByPj {
+  return {
+    ...historyByPj,
+    [currentPjId]: newHistory,
+  }
 }
