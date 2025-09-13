36a9fd6ce3af2be129759708d5c2a3764e0e4ce5
yopi416 <yoppytennis416@gmail.com>
2025-09-13 23:33:26 +0900

    ルーティング設定(認証はダミー) #67

--- src/pages/TestSomething.tsx
+++ src/pages/TestSomething.tsx
@@ -2,28 +2,13 @@
 // import { useCallback, useState } from 'react'
 // import { TestSomething2 } from './TestSomething2'
 
-// const DisplayCurrentNum = () => {
-//   const [state, setState] = useState<number>(0)
-//   const clickHandler = () => {
-//     setState((prev) => prev + 1)
-//   }
-
-//   console.log("I am DisplaycurrentNum")
-
-//   return (
-//     <div>
-//       <Button onClick={clickHandler}>
-//         Button
-//       </Button>
-//       <p>current number is {state}</p>
-//     </div>
-//   )
-
-// }
+import { Outlet } from 'react-router'
 
 export const TestSomething = () => {
-  const visilble: boolean = false
-  const htmlTag = visilble ? <div></div> : <div>invisible</div>
-
-  return htmlTag
+  return (
+    <>
+      <h1>This is TestSomething!!</h1>
+      <Outlet />
+    </>
+  )
 }
