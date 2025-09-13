911099309e3d6eb633115e38c46ccd7abbea9f2f
yopi416 <yoppytennis416@gmail.com>
2025-09-13 14:58:38 +0900

    backupファイルを削除 #62

--- src/pages/TestSomething2.tsx
+++ src/pages/TestSomething2.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/ui/button'
 import { memo } from 'react'
 
+// console.log("dummy")
+
 type Props = {
   // setState: (value: React.SetStateAction<number>) => void
   hello: () => void
